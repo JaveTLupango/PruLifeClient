e64b5cc04f3e6ebf2a2a07f217a15751fa59bfe6
Jave Lupango <pangga.jave@gmail.com>
2023-04-01 11:24:29 +0800

    routing of request url

--- prulifeclientweb/src/app/app-routing.module.ts
+++ prulifeclientweb/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { GenerateRequentClientComponent } from './generate-requent-client/generate-requent-client.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'login', component:LoginComponent}
+  {path: 'login', component:LoginComponent},
+  {path: 'generaterequesturl', component:GenerateRequentClientComponent}
 ];
 
 @NgModule({
