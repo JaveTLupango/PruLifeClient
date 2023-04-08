03c6c699560390d6daed30be2221f11e8ce5ab46
Jave Lupango <pangga.jave@gmail.com>
2023-04-08 19:05:08 +0800

    DB migration

--- prulifeclientapi/database/migrations/2023_04_08_060449_create_request_urls_table.php
+++ prulifeclientapi/database/migrations/2023_04_08_060449_create_request_urls_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateRequestUrlsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('request_urls', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('username')->unique();
-            $table->string('is_verified')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->integer('user_role');
+            $table->integer('user_id');
+            $table->string('receipt_email');
             $table->boolean('is_active');
             $table->boolean('is_deleted');
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('gen_id');
             $table->timestamps();
         });
     }
@@ -36,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('request_urls');
     }
 }
