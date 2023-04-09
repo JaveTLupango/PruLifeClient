b834431a2de808654cc664768485d483270e18dc
Jave Lupango <pangga.jave@gmail.com>
2023-04-09 17:17:21 +0800

    login and logout validation

--- prulifeclientweb/src/app/login/login.component.ts
+++ prulifeclientweb/src/app/login/login.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm, NgModel } from '@angular/forms';
 import { Login } from '../Model/login.model';
+import { LoginService } from '../shared/services/auth/login.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-login',
@@ -8,13 +13,23 @@ import { Login } from '../Model/login.model';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  
+constructor(private http: HttpClient,private router: Router)
+{
+
+}
+
   formValueLogin : Login = new Login;
-  username: string = '';
+  email: string = '';
   password: string = '';
+  login_services : LoginService = new LoginService(this.http, this.router);
 
   onSubmit() {
     console.log('Login clicked');
-    console.log('Username:', this.username);
+    console.log('Username:', this.email);
     console.log('Password:', this.password);
+    this.formValueLogin.password = this.password;
+    this.formValueLogin.email = this.email;
+    this.login_services.login(this.formValueLogin);
   }
 }
