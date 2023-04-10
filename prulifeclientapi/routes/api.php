f9c1fd1c11a198b0408d25750cbac6f157fd38f8
Jave Lupango <pangga.jave@gmail.com>
2023-04-10 12:00:20 +0800

    route

--- prulifeclientapi/routes/api.php
+++ prulifeclientapi/routes/api.php
@@ -22,6 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/auth/register', [AuthController::class,'register']);
 Route::post('/auth/login', [AuthController::class,'login']);
+
 Route::get('/auth/users', [AuthController::class,'listofuser'])->middleware('auth:sanctum');
 Route::post('/auth/logout', [AuthController::class,'logout'])->middleware('auth:sanctum');
 Route::post('/request/create', [CreateRequestURLController::class,'create'])->middleware('auth:sanctum');
