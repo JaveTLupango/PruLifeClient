a44ff73bafd419b81d206c3c23d2be2c7d1c26a1
Jave Lupango <pangga.jave@gmail.com>
2023-04-16 18:04:47 +0800

    usertermconcent

--- prulifeclientweb/src/app/client-personal-info/client-personal-info.component.ts
+++ prulifeclientweb/src/app/client-personal-info/client-personal-info.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import PersonalInfoService from '../shared/services/policy/personal-info.service'; 
+
 
 @Component({
   selector: 'app-client-personal-info',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./client-personal-info.component.css']
 })
 export class ClientPersonalInfoComponent {
-
+  constructor(private router: Router, private http: HttpClient, private route: ActivatedRoute){}
+  Req_id: number = 0;
+  rowCount : number = 0;
+  isDisabled: boolean = false;
+  personalInfo: PersonalInfoService = new PersonalInfoService(this.http);
+  ngOnInit()
+  {
+    this.Req_id = this.route.snapshot.params['id'];
+    
+  }
 }
